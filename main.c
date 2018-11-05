9dab8c16ca44049b8a4bdaad8d6bd4c91a48edb8
MDaineko <42941961+MDaineko@users.noreply.github.com>
2018-11-05 09:59:11 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <conio.h>
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a[20] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+	int n = 20;
+	unsigned i, j;
+	int max, min, imax, imin;
+	
+	max = a[0];
+	for (i=1; i<n; i++){
+		if(a[i]>max){
+			max = a[i];
+		}
+	}
+	printf("max element is %d", max);
+	getch();
+	
+	min = a[0];
+	for (i=1; i>n; i++){
+		if(a[i]<min){
+			min = a[i];
+		}
+	}
+	printf("\nmin element is %d", min);
+	getch();
+	
+	for(i=0; i<n; i++){
+		if(a[i]<1)
+			for(i=0; i<n-1; i++){
+			a[i] = a[i+1];	
+	}
+}
+}
