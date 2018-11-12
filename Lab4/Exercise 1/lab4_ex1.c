880e7b992d5a7cb0a78cbc852eb7aa74d18b016f
MDaineko <42941961+MDaineko@users.noreply.github.com>
2018-11-12 09:32:25 +0300

    Add files via upload

--- Lab4/Exercise 1/lab4_ex1.c
+++ Lab4/Exercise 1/lab4_ex1.c
@@ -1,35 +1,44 @@
-#include <conio.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+	int max_elem(int *arr, const int K){ 
+	int max; int i;
+	for(i = 0; i < K; ++i)
+	if(arr[i] > max)
+	max = arr[i];
+}
 
-int main(int argc, char *argv[]) {
-	int a[20] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-	unsigned i, j;
-	int n, max, min;
+	int min_elem(int *arr, const int K){ 
+	int min; int i;
+	for(i = 0; i < K; ++i)
+	if(arr[i] < min)
+	min = arr[i];
+	return min;
+}
+
+	int main()
+{
+	setlocale(LC_ALL, "Russian");
+	srand(time(NULL));
+	const int K = 10;
+	int arr[K];
+	int i;
 	
-	max = a[0];
-	for (i=1; i<20; i++){
-		if(a[i]>max){
-			max = a[i];
-		}
+	printf("Исходный массив: ");
+	for(i = 0; i < K; ++i){
+		arr[i] = rand() % K;
+		printf("%d ", arr[i]);
 	}
-	printf("max element is %d", max);
-	getch();
-	min = a[0];
-	for (i=1; i>20; i++){
-		if(a[i]<min){
-			min = a[i];
-		}
+	printf("\n");
+	
+	for(i = 0; i < K; ++i){
+		if(arr[i] == max_elem(arr, K)) arr[i] = 0;
+		if(arr[i] == min_elem(arr, K)) arr[i] = 0;
 	}
-	printf("\nmin element is %d", min);
-	getch();
-	for (i=0;i<20;i++);
-		if(a[i]>max){
-			for(i+=1;i>20;i++)
-			a[i-1]=a[i];
-		printf("%d",&a[i]);
-		}
-	n--;
-	i--;	
+	
+	printf("Модифицированный массив: ");
+	for(i = 0; i < K; ++i)
+		printf("%d ", arr[i]);
 }
