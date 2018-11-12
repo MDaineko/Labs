94bd4beece6a12a0a1e0c78551c7542ebae26bf5
MDaineko <42941961+MDaineko@users.noreply.github.com>
2018-11-12 08:37:09 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,30 +1,13 @@
-#include <conio.h>
 #include <stdio.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	int a[20] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-	int n = 20;
-	unsigned i, j;
-	int max, min, imax, imin;
-	
-	max = a[0];
-	for (i=1; i<n; i++){
-		if(a[i]>max){
-			max = a[i];
-		}
-	}
-	printf("max element is %d", max);
-	getch();
-	
-	min = a[0];
-	for (i=1; i>n; i++){
-		if(a[i]<min){
-			min = a[i];
-		}
-	}
-	printf("\nmin element is %d", min);
-	getch();
-
+	printf("Enter the initial number of inhabitants: ");
+	int n, i;
+	scanf("%d", &n);
+	int r = 3 * n;
+	for(i = 1; i < r; ++i)
+		n += 1/n; 
+	printf("Number of residents will tripple after %d\n", i);
+	return 0;
 }
